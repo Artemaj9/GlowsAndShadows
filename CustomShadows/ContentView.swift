9c587fdb036c0333e13e757235d5b97a7b12c95f
Artem <moiseev@physics.msu.ru>
2023-07-21 11:20:43 +0300

    Glow and multicolor glow effect

--- CustomShadows/ContentView.swift
+++ CustomShadows/ContentView.swift
@@ -7,15 +7,46 @@
 
 import SwiftUI
 
+
+extension View {
+    func glow(color: Color = .red, radius: CGFloat = 20) -> some View {
+        self
+            .overlay(self.blur(radius: radius/6))
+            .shadow(color: color, radius: radius/3)
+            .shadow(color: color, radius: radius/3)
+            .shadow(color: color, radius: radius/3)
+    }
+}
+
+extension View {
+    func multicolorGlow() -> some View {
+        ForEach(0..<2) { i in
+            Rectangle()
+                .fill(AngularGradient(gradient: Gradient(colors: [.red,.yellow,.green,.blue,.purple,.red]), center: .center))
+                .frame(width: 400, height: 400)
+                .mask(self.blur(radius: 20))
+                .overlay(self.blur(radius: 5 - CGFloat(i*5)))
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+//            Circle()
+//                .fill(Color.white)
+//                .glow(color: .blue, radius: 36)
+              
+            Text("Hello World")
+                .font(.system(size: 96, weight: .black, design: .rounded))
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                .frame(width: 400, height: 300)
+                .multicolorGlow()
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
