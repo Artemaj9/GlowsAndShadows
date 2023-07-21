6859b5cbe195742cfc667136b3158838428cd724
Artem <moiseev@physics.msu.ru>
2023-07-21 10:57:50 +0300

    Initial Commit

---
+++ CustomShadows/CustomShadowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomShadowsApp.swift
+//  CustomShadows
+//
+//  Created by Artem on 21.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomShadowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
